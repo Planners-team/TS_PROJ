e266c8fe3ad07960d8b847952ff5d0f6da5a563f
Yarden Elgali <planners.heruko@gmail.com>
2022-10-30 08:57:25 +0200

    import_export
    import_export

--- TS_proj/settings.py
+++ TS_proj/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'import_export',
     'django_filters',
     'ts_system',
 ]
