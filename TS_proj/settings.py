c6539bdc3511fe0958c04064b3a1128ed8a60f51
Yarden Elgali <planners.heruko@gmail.com>
2022-10-31 09:41:37 +0200

    STATICFILES

--- TS_proj/settings.py
+++ TS_proj/settings.py
@@ -138,4 +138,9 @@ CRONJOBS = [
 LOGIN_REDIRECT_URL = '/dashboard/'
 LOGOUT_REDIRECT_URL = '/'
 
-DISABLE_COLLECTSTATIC=1
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
