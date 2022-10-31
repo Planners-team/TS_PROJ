5dd61fab73ae64b8f8849cdfc59b3e8104fda190
Yarden Elgali <planners.heruko@gmail.com>
2022-10-31 14:32:19 +0200

    nltk

--- TS_proj/settings.py
+++ TS_proj/settings.py
@@ -140,7 +140,7 @@ LOGOUT_REDIRECT_URL = '/'
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'C:/Users/yelgali/TS_PROJ/ts_system/static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
